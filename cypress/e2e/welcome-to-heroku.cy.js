6b42ded0ed8d9848a8eefc0b6cb14afd1d8d32a1
Vlad Vira <vladvira@192.168.1.137>
2025-06-21 12:43:25 +0300

    Remove unnecesary dependecies from package.json

--- cypress/e2e/welcome-to-heroku.cy.js
+++ cypress/e2e/welcome-to-heroku.cy.js
@@ -1,5 +1,10 @@
 describe("Navigate to heroku app",()=>{
     it("Navigate to Heroku main page",()=>{
         cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('.heading').should('contain',"Welcome to the-internet")
+    })
+    it("Should contain sub title named 'Avilable Examples' ",()=>{
+        cy.visit('https://the-internet.herokuapp.com/')
+        cy.get('h2').should("contain","Available Examples")
     })
 })
