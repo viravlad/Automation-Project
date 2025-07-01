b8433774aa127a0a1a812c373c10ae9b242c7f65
Vlad Vira <vladvira@192.168.1.137>
2025-07-01 14:52:02 +0300

    Add test's for dynamically-loaded-page-elemts

--- cypress/e2e/dynamically_loaded_page_elements.cy.js
+++ cypress/e2e/dynamically_loaded_page_elements.cy.js
@@ -12,6 +12,18 @@ describe("Element on page that is hidden and removed",()=>{
         cy.get('.example > :nth-child(2)').should("contain","Example 1: Element on page that is hidden")
     })
 
+    it("The element is hidden but on the page",()=>{
+        cy.get('[href="/dynamic_loading/1"]').click()
+        cy.get("#finish h4").should("exist")
+    })
+
+    it("Loading spinner should be visible until the text is displayed",()=>{
+        cy.get('[href="/dynamic_loading/1"]').click()
+        cy.get('button').click()
+        cy.get("#loading").should('be.visible')
+
+    })
+
     it("Should make visible the element after click on Start button",()=>{
         cy.get('[href="/dynamic_loading/1"]').click()
         cy.get('button').click()
@@ -19,13 +31,23 @@ describe("Element on page that is hidden and removed",()=>{
         cy.get('#finish h4', { timeout: 10000 }).should("be.visible").and("contain","Hello World!")
     })
 
-    it("Should hide and not delete the element after click on Start button",()=>{
-        cy.get('[href="/dynamic_loading/1"]').click()
+    it("Should navigate with success on Dynamic Loading Example 2 page",()=>{
+        cy.get('[href="/dynamic_loading/2"]').click();
+        cy.get('.example > :nth-child(2)').should('contain',"Example 2: Element rendered after the fact")
+    })
+
+    it("Loading spinner should be visible on DYnamic Loading page on clicking button",()=>{
+        cy.get('[href="/dynamic_loading/2"]').click();
         cy.get('button').click()
-        
+        cy.get("#loading").should('be.visible')
+
+    })
+
+    it("Dynamic Loading Example 2",()=>{
+        cy.get('[href="/dynamic_loading/2"]').click();
+        cy.get("#finish h4").should("not.exist");
+        cy.get('button').click()
         cy.get('#finish h4', { timeout: 10000 }).should("be.visible").and("contain","Hello World!")
-        cy.get('button').click()
-        cy.get('#finish h4', { timeout: 10000 }).should("not.be.visible")
     })
 
 })
